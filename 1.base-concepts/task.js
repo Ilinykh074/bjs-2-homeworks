b6ee275e3ae85cd2d86e3ea1d06dc82ea7c647f7
Ivan <ilinykh074@gmail.com>
2023-04-13 23:32:06 +0500

    -a

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,11 +2,8 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let discriminant = (b ** 2) - 4 * a * c;
-  console.log (discriminant);
 	if (discriminant > 0) {
-    console.log(discriminant);
-    console.log();
-		arr.push((-b + Math.sqrt(discriminant)) / (2 * a))
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a))
 		arr.push((-b - Math.sqrt(discriminant)) / (2 * a))
 	} else if (discriminant === 0) {
 		arr.push(-b / (2 * a))
@@ -16,10 +13,10 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 	
-  if ((isNaN(percent)) ||(isNaN(contribution)) || (isNaN(amount)) || (isNaN(countMonths))) {
+  if ((isNaN(percent)) || (isNaN(contribution)) || (isNaN(amount)) || (isNaN(countMonths))) {
 		return false;
   }
-
+  
   percent = percent / 100 / 12;
   let bodyCredite = amount - contribution;
   let payment = bodyCredite * (percent + (percent / (((1 + percent) ** countMonths) - 1)));
